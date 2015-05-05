e634de1c72fe76cd0fa37bc49d7b9b77e1bbfb08
lum1nary <therock95@i.ua>
2015-05-05 20:00:17 +0300

    updates

---
+++ Monopoly-South-Park-Edition/Assets/Scripts/Location.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Location : MonoBehaviour {
+	
+	Player Owner { get; set;}
+	Sprite sprite { get; set;}
+
+
+
+}
+
+public enum Group {
+	Activity = 0, 
+	Purple = 1, 
+	LightBlue = 2, 
+	Pink = 3,
+	Orange = 4,
+	Red = 5,
+	Yellow = 6,
+	Green = 7,
+	Blue  = 8,
+	Social = 9
+}
---
+++ Monopoly-South-Park-Edition/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player
+{
+	uint   Id        { get; set;}
+	//uint   Position  { get; set;}
+	int    Money     { get; set;}
+	string Name      { get; set;}
+	Sprite Avatar    { get; set;}
+	Location[] Cards { get; set;}
+	public Player(uint id, int money, string name, Sprite avatar)
+	{
+		Id = id;
+		Money = money;
+		Name = name;
+		Avatar = avatar;
+
+	}
+}
